5e4aee037c0e8d29e3dee4d7b5d653850f90d859
dylan <dylanabrewer@gmail.com>
2022-04-13 07:45:08 -0600

    cleaning up GPU script files and adding script path to script calls in run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
+SCRIPT_PATH=scripts
+
 ####################################################################################################
 # EXECUTE CPU SIZE RUNS
 ####################################################################################################
-./run_cpu.sh
+echo RUNNING CPU...
+./$SCRIPT_PATH/run_cpu.sh
 
 ####################################################################################################
 # EXECUTE GPU SIZE RUNS
 ####################################################################################################
+echo RUNNING PASCAL...
 GPU_TYPE=pascal
-run_gpu run_gpu.sh < input_pascal
+run_gpu $SCRIPT_PATH/run_gpu.sh < input_pascal
+echo RUNNING VOLTA...
 GPU_TYPE=volta
-run_gpu run_gpu.sh < input_volta
+run_gpu $SCRIPT_PATH/run_gpu.sh < input_volta
+echo RUNNING AMPERE...
 GPU_TYPE=ampere
-run_gpu run_gpu.sh < input_ampere
+run_gpu $SCRIPT_PATH/run_gpu.sh < input_ampere
 
 ####################################################################################################
 # EXECUTE GPU DIM RUNS
 ####################################################################################################
-run_gpu run_gpu_dim.sh < input_ampere
+echo RUNNING DIM...
+run_gpu $SCRIPT_PATH/run_gpu_dim.sh < input_ampere
 
 ####################################################################################################
 # VERIFY CPU/GPU CORRECTNESS
 ####################################################################################################
-# Compare Voltage CPU Checksum to Comparable GPU (Pascal, Volta, Ampere) Checksums 
+# Compare Voltage CPU Checksum to Comparable GPU (Pascal, Volta, Ampere) Checksums
+echo COMPARING VSUMS 
 for cpu_name in output/cpu/*.vsum
 do
     # Compare voltage checksums
@@ -34,7 +42,9 @@ do
         echo $($CMD) >> output/compare.txt
     done
 done
+
 # Compare Field CPU Checksum to Comparable GPU (Pascal, Volta, Ampere) Checksums 
+echo COMPARING ESUMS
 for cpu_name in output/cpu/*.esum
 do
     # Compare voltage checksums
--- scripts/run_gpu_volta.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./run_gpu.sh volta
