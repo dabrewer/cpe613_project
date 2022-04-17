65aefc458795d10aacc2cd35bad27ae6927974fd
dylan <dylanabrewer@gmail.com>
2022-04-16 20:28:21 -0600

    add another checksum script

---
+++ scripts/calc_checksums.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# CALC CPU CHECKSUMS
+shasum $(ls output/cpu/*.v) >> output/cpu/cpu.vsum
+shasum $(ls output/cpu/*.e) >> output/cpu/cpu.esum
+
+# CALC PASCAL CHECKSUMS
+shasum $(ls output/gpu/pascal/*.v) >> output/gpu/pascal/pascal.vsum
+shasum $(ls output/gpu/pascal/*.e) >> output/gpu/pascal/pascal.esum
+
+# CALC VOLTA CHECKSUMS
+shasum $(ls output/gpu/volta/*.v) >> output/gpu/volta/volta.vsum
+shasum $(ls output/gpu/volta/*.e) >> output/gpu/volta/volta.esum
+
+# CALC AMPERE CHECKSUMS
+shasum $(ls output/gpu/ampere/*.v) >> output/gpu/ampere/ampere.vsum
+shasum $(ls output/gpu/ampere/*.e) >> output/gpu/ampere/ampere.esum
+
+# PRINT CPU CHECKSUMS
+cat $(ls output/cpu/*.vsum)
+cat $(ls output/cpu/*.vsum)
+
+# PRINT GPU CHECKSUMS
+cat $(ls output/gpu/*/*.vsum)
+cat $(ls output/gpu/*/*.esum)
--- scripts/run_checksums.sh
+++ scripts/run_checksums.sh
@@ -10,23 +10,16 @@ export SAVE_OUTPUT=y
 ####################################################################################################
 echo RUNNING CPU...
 ./$SCRIPT_PATH/run_cpu.sh
-shasum $(ls output/cpu/*.v) >> output/cpu/cpu.vsum
-shasum $(ls output/cpu/*.e) >> output/cpu/cpu.esum
 
 echo RUNNING PASCAL...
 export GPU_TYPE=pascal
 run_gpu $SCRIPT_PATH/run_gpu.sh < $SCRIPT_PATH/input_pascal
-shasum $(ls output/gpu/pascal/*.v) >> output/gpu/pascal/pascal.vsum
-shasum $(ls output/gpu/pascal/*.e) >> output/gpu/pascal/pascal.esum
 
 echo RUNNING VOLTA...
 export GPU_TYPE=volta
 run_gpu $SCRIPT_PATH/run_gpu.sh < $SCRIPT_PATH/input_volta
-shasum $(ls output/gpu/volta/*.v) >> output/gpu/volta/volta.vsum
-shasum $(ls output/gpu/volta/*.e) >> output/gpu/volta/volta.esum
 
 echo RUNNING AMPERE...
 export GPU_TYPE=ampere
 run_gpu $SCRIPT_PATH/run_gpu.sh < $SCRIPT_PATH/input_ampere
-shasum $(ls output/gpu/ampere/*.v) >> output/gpu/ampere/ampere.vsum
-shasum $(ls output/gpu/ampere/*.e) >> output/gpu/ampere/ampere.esum
+
